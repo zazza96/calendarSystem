f1f79fcf70b4643c33c4ece2ee1112a7a4ee8afa
zazza96 <zazza96@gmail.com>
2022-10-17 23:14:41 +0100

    initiated project and JSON file created alongside with HTML

---
+++ app/src/assets/js/app.js
@@ -0,0 +1,12 @@
+// Font Awesome
+import '@fortawesome/fontawesome-free';
+
+// Styles
+import '../scss/main.scss';
+
+// // Own modules
+// import getBooks from './modules/getBooks';
+// import clickBook from './modules/clickBook';
+
+// getBooks();
+// clickBook();
