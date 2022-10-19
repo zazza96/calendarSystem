b28cf9064034307c97329c9f962b22d84d0a4a0e
zazza96 <zazza96@gmail.com>
2022-10-19 23:52:09 +0100

    a lot of styles added, fixed a few structural issues on HTML and js

--- app/src/assets/js/modules/createCalendar.js
+++ app/src/assets/js/modules/createCalendar.js
@@ -10,12 +10,13 @@ export default ()=>{
     for (const obj of calendar) {
         //create date section
         let thisdate = obj.date;
-        dateContainer.insertAdjacentHTML('beforeend',`<div class="date" data-date="${thisdate}"><span>${thisdate}</span></div>`);
+        let thisday = obj.day;
+        dateContainer.insertAdjacentHTML('beforeend',`<div class="date-container" data-date="${thisdate}"><span class="day">${thisday}</span><span class="date">${thisdate}</span></div>`);
         classesContainer.insertAdjacentHTML('beforeend',`<div class="thisdaysLesson lessondate-${thisdate}"></div>`);
 
         //loop through second level 
         for (const lesson of obj.lessons) {
-            document.querySelector('.lessondate-'+thisdate).insertAdjacentHTML('beforeend',`<div class="class_container flex" data-name="${lesson.name}" data-id="${lesson.id}"><div class="class-details flex-left"><h3>${lesson.name}</h3><div class="class-time"><span><i class="fa fa-light fa-clock"></i></span><span class="time">${lesson.time}</span></div><p class="location">${lesson.location}</p></div><div class="flex-right"><div class="booking_btn-container"><button class="booking_btn">BOOK</button></div><div class="spaces"><p>${lesson.spaces}</p></div></div></div>`);
+            document.querySelector('.lessondate-'+thisdate).insertAdjacentHTML('beforeend',`<div class="class_container flex" data-name="${lesson.name}" data-id="${lesson.id}"><div class="class-details flex-left"><h3>${lesson.name}</h3><div class="class-time"><span><i class="fa fa-light fa-clock"></i></span><span class="time">${lesson.time}</span></div><p class="location">${lesson.location}</p></div><div class="flex-right"><div class="booking_btn-container"><button class="booking_btn">BOOK</button></div><div class="spaces"><p>${lesson.spaces} SPACES LEFT</p></div></div></div>`);
             
         }
         
